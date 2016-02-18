8f3092ee38037f4c8eedc2ca01e9d8bc49af1795
Nathan Lea <nathan.lea@okstate.edu>
2016-02-18 17:14:51 -0600

    Changed script to take every minute

--- takepicture.sh
+++ takepicture.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
+COUNTER=0
 DATE=$(date +%Y-%m-%d_%H%M)
 
-mkdir webcam
-
-sudo mount -t tmpfs tmpfs webcam
-
-fswebcam -r 640x480 --no-banner /home/pi/SecureCameraServer/webcam/toSend.jpg
-#call sript to send 
-python SSLtestWebcam.py
-
-sudo umount webcam
-
-rm -r webcam
+while [ $COUNTER -le 5 ]
+do
+	mkdir webcam
+	sudo mount -t tmpfs tmpfs webcam
+	fswebcam -r 640x480 /home/pi/SecureCameraServer/webcam/toSend.jpg
+	#call sript to send 
+	python SSLtestWebcam.py
+	sudo umount webcam
+	rm -r webcam
+	sleep 60
+done
