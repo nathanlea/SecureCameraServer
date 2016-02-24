94f04dfc12a4a131b9dc4d4c8c908fca1c7c875c
Kevin Seitz <kseitz@okstate.edu>
2016-02-24 12:13:07 -0600

    updated the paths to full pathing for use with crontab

--- SSLtestWebcam.py
+++ SSLtestWebcam.py
@@ -1,15 +1,22 @@
-import socket, ssl, pprint, sys
+import socket, ssl, pprint, sys, picamera
+from time import sleep
 
 def get_bytes_from_file(filename):
     return (open(filename, "rb")).read()
 
 if __name__ == '__main__':
 
+   # camera=picamera.PiCamera()
+   # camera.start_preview()
+   # sleep(5)
+   # camera.capture("/home/pi/SecureCameraServer/webcam/toSend.jpg")
+   # camera.stop_preview()
+
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     ipnathan = '139.78.71.59'
     portnathan = 10023
     #req cert from server
-    ssl_sock = ssl.wrap_socket(s, ca_certs="server.crt", cert_reqs=ssl.CERT_REQUIRED)
+    ssl_sock = ssl.wrap_socket(s, ca_certs="/home/pi/SecureCameraServer/server.crt", cert_reqs=ssl.CERT_REQUIRED)
 
     ssl_sock.connect((ipnathan, portnathan))
 
@@ -19,7 +26,7 @@ if __name__ == '__main__':
 
     #simple http request-- use httplib in actual
     #ssl_sock.write(get_bytes_from_file(str(sys.argv)))
-    ssl_sock.write(get_bytes_from_file('webcam/toSend.jpg'))
+    ssl_sock.write(get_bytes_from_file("/home/pi/SecureCameraServer/webcam/toSend.jpg"))
     #read a data chunk, not necessarily all returned by server
     #data = ssl_sock.read()
 
