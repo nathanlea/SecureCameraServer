7ee7add24f478340da1ba8f748acd6d584994aba
Kevin Seitz <kseitz@okstate.edu>
2016-02-24 12:14:53 -0600

    newest version of takepicture script, for use with crontab

--- takepicture.sh
+++ takepicture.sh
@@ -3,15 +3,22 @@
 i="0"
 DATE=$(date +%Y-%m-%d_%H%M)
 
-while [ $i -lt 4 ]
-do
+#while [ $i -lt 1 ]
+#do
+#mount a folder into RAM to store the picture
 mkdir -p /home/pi/SecureCameraServer/webcam
 sudo mount -t tmpfs tmpfs /home/pi/SecureCameraServer/webcam
-raspistill -o /home/pi/SecureCameraServer/webcam/toSend.jpg
+
+#take the picture
+raspistill -vf -o /home/pi/SecureCameraServer/webcam/toSend.jpg
+
 #call sript to send 
-python SSLtestWebcam.py
-sleep 10
+/usr/bin/python /home/pi/SecureCameraServer/SSLtestWebcam.py
+
+#remove the picture from RAM
 sudo umount /home/pi/SecureCameraServer/webcam
 rm -r /home/pi/SecureCameraServer/webcam
+
 echo "script complete"
-done
+#sleep 10
+#done
