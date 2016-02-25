bf4c092c881f17aeff704a7894d2135f215724f8
root <root@raspberrypi.ceat.okstate.edu>
2016-02-25 23:24:06 +0000

    test script

---
+++ test.py
@@ -0,0 +1,9 @@
+import receive
+
+if __name__ == '__main__':
+	receive.receive("2016022505330", "test.jpg")
+	receive.receive("2016022505340", "test.jpg")
+	receive.receive("2016022506330", "test.jpg")
+	receive.receive("2016022605330", "test.jpg")
+	receive.receive("2016032505330", "test.jpg")
+	receive.receive("2017022505330", "test.jpg")
